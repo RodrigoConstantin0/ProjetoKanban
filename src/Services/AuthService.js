2ca9a1adbe56569257af9d3e792d02897c16a202
Juliana Maria de Sousa Mesquita <julianamariasousamesquita@id.uff.br>
2024-02-18 16:59:41 -0300

    feat: adicionando lógica de autenticação de serviço isAuthenticated, login e logout

--- src/Services/AuthService.js
+++ src/Services/AuthService.js
@@ -1,1 +1,16 @@
-// AuthService = true    
+const AuthService = {
+    isAuthenticated: () => {
+      const userLoggedIn = localStorage.getItem('userLoggedIn');
+      return userLoggedIn === 'true' || false;
+    },
+    login: (email) => {
+        localStorage.setItem('userLoggedIn', 'true');
+        localStorage.setItem('email', email);
+    },
+    logout: () => {
+      localStorage.removeItem('userLoggedIn');
+      localStorage.removeItem('email');
+    },
+  };
+  
+  export default AuthService;
