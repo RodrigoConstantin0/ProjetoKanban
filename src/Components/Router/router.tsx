72ecbbb3b0ef0ed079966f6586be9ad617a2a564
Dayana Mariano <dayanaraquelle@gmail.com>
2024-02-19 14:28:06 -0300

    feat: Página Kanban

--- src/Components/Router/router.tsx
+++ src/Components/Router/router.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Login } from "../../Pages/Login";
+import Kanban from "../../Pages/Kanban";
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Login/>}/>
+                <Route path="/Kanban" element={<Kanban/>}/>
+
             </Routes>
         </BrowserRouter>
     )
