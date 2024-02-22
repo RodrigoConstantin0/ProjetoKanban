137026d82d19926394d12459f9dc504d54cd5e74
Rodrigo Constantino <rodrigoconstantino2@gmail.com>
2024-02-21 23:31:10 -0300

    feat: modificação env

--- src/Pages/Register/index.tsx
+++ src/Pages/Register/index.tsx
@@ -40,7 +40,7 @@ const Register: React.FC = () => {
     localStorage.setItem("users", JSON.stringify(existingUsers));
     
     AuthServiceProps.login(email);
-    localStorage.setItem('userName', name); // Adicionando o nome do usuário no localStorage
+    localStorage.setItem('userName', name);
     alert("Cadastro realizado com sucesso!");
     
     const isLoggedIn = AuthServiceProps.isAuthenticated();
@@ -61,7 +61,7 @@ const Register: React.FC = () => {
 
       <div className="register-global">
         <form className="form-register" onSubmit={handleRegister}>
-          <h1>Cadastre - se</h1>
+          <h1 className="h1Register">Cadastre - se</h1>
           <label htmlFor="name">
             <input
               type="text"
