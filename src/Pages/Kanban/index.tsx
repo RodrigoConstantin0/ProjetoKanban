1d07aa8cb0d520613eedae0e53502a7ee4b5c069
Rodrigo Constantino <rodrigoconstantino2@gmail.com>
2024-02-23 14:50:43 -0300

    feat: ajuste nas responsividade

--- src/Pages/Kanban/index.tsx
+++ src/Pages/Kanban/index.tsx
@@ -16,18 +16,20 @@ function Kanban() {
 
   return (
     <>
+    <div className='container'>
       <header className="header-kanban">
         <div className="wrapperlogo">
           <img src="/src/Assets/KanbanLogo.png" alt="Logo" />
         </div>
         <h2 className="h2header"> Kanban </h2>            
         <nav>
-          {isLoggedIn && (<p>Bem-vindo, {userName} <Button titulo="Sair" tipo='logout' onClick={handleLogout} /></p>)}
+          {isLoggedIn && (<p className='style-bemVindo'>Bem-vindo, {userName} <Button titulo="Sair" tipo='logout' onClick={handleLogout} estilo='kanban-button'/></p>)}
         </nav>
       </header>
     <App/>
-      <footer>
-        <p>Feito pelo grupo 2 - Formação Front-end da Ada Tech 2024</p>
+    <footer className='footer-style'>
+        <div className='footer-left'> 
+        <p className='P-footer'>Feito pelo grupo 2 - Formação Front-end da Ada Tech 2024</p>
         <a
           href="https://github.com/RodrigoConstantin0/ProjetoKanban"
           target="_blank"
@@ -36,9 +38,12 @@ function Kanban() {
             src="/src/Assets/logoGithub.svg"
             alt="logo-gitbub"
             id="logoGithut"
+            className='img-footer'
           />
         </a>
-      </footer>
+        </div>
+    </footer>
+      </div>
     </>
   );
 }
